9474f263d22be6afdb32d977d30751abdb206330
SondosSamii <sondos379@gmail.com>
2019-09-20 20:02:47 +0200

    Add animating counter

--- Exam1/js/index.js
+++ Exam1/js/index.js
@@ -1,6 +1,18 @@
-let navItem = document.getElementsByClassName("nav-item");
+$(document).ready(function() {
+    $(".navbar-nav li").click(function() {
+        $(this).addClass("current");
+        $(".navbar-nav li").not($(this)).removeClass("current");
+    });
 
-$(".navbar-nav li").click(function() {
-    $(this).addClass("current");
-    $(".navbar-nav li").not($(this)).removeClass("current");
+    $('.counter h3').each(function() {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function(now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
 });
